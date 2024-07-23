cbe4376fba952158ab3715bf9a37d9f7b13fea09
Dereck Sebastian Muñoz <dsmunozc@indracompany.com>
2024-07-22 19:47:10 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./infrastructure/http/routes/AuthRoutes');
+const authRoutes = require('./infrastructure/http/routes/authRoutes');
 const comicRoutes = require('./infrastructure/http/routes/ComicRoutes');
 
 dotenv.config();
